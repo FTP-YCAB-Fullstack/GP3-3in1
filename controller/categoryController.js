bbf995bb7ad864a518a51f610c9604b385f62f9b
AlimMakruf <alimm.makruf@gmail.com>
2021-10-01 21:09:26 +0700

    create get all category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,22 +1,20 @@
 const {Category} = require ('../models')
+const CategoryModel = require('../models').category
 
-const categoryCon = { 
-    getAll : async (req,res,next) => {
+class CategoryController {
+    static getAll  = async (req,res,next) => {
         try {
             const data = await Category.findAll()
 
             if(!data){ 
                 return next ({code: 404 , message: 'Data not found'})
             }
-            res.status(200).json({
-                status : 'success',
-                data
-            })
+            res.status(200).json(data)
         } catch (err) {
             next({code: 500, message: err.message || 'Internal Server Error'})
         }
 
     }
-
-    
 }
+
+module.exports = CategoryController
